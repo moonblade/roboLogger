81813352e820597f4c70a368d1b5f43b76eacd13
Nisham <moonblade168@gmail.com>
2016-03-01 00:44:54 +0530

    timeline fucking works

--- backend/application/controllers/Users.php
+++ backend/application/controllers/Users.php
@@ -5,35 +5,14 @@ class Users extends CI_Controller {
         {
                 // $this->load->view('pages/manualPicker');
 		$data['chart_data'] = json_decode('[{"year":1,"time":10},{"year":2,"time":20},{"year":3,"time":30},{"year":4,"time":40}]',True);
-                $data['chart_data']=$this->getData();
+                $data['chart_data']=$this->getPeople();
                 $this->load->view('pages/chart',$data);
-        }
-
-        private function getData()
-        {
-    	$hours = 5;
-	        $now=new DateTime();
-            $now->add(new DateInterval('PT4H30M'));
-            $from=new DateTime();
-            $from->add(new DateInterval('PT4H30M'));
-            $from->sub(new DateInterval('PT'.$hours.'H'));
-            $iterator=$from;
-            for($i=0; $i<=$hours;$i++)
-            {
-                                $returnValues=$this->getPeople();
-                        $result['devices']=$returnValues;
-                        // $result['time']=$i;
-                        $result['time']=$iterator->getTimeStamp();
-                        $result['count']=count($result['devices']);
-			            $toReturn[]=$result;
-                        $iterator->add(new DateInterval('PT1H'));
-            }
-            return($toReturn);
         }
 
         private function getPeople($from=24)
         {
-                $query = $this->db->query('select name,userMacId,userLoginTime, REPLACE(userLogoutTime,"0000-00-00 00:00:00",NOW()) from log,loggerUsers where log.userMacId = loggerUsers.macId');
+                $query = $this->db->query('select REPLACE(name,"Anonymous",userMacId) as name,UNIX_TIMESTAMP(userLoginTime) as userLoginTime, UNIX_TIMESTAMP(REPLACE(userLogoutTime,"0000-00-00 00:00:00",NOW())) as userLogoutTime from log,loggerUsers where log.userMacId = loggerUsers.macId
+');
                 // header('Content-Type: application/json');
                 return $query->result();
         
--- backend/application/views/pages/chart.php
+++ backend/application/views/pages/chart.php
@@ -1,27 +1,47 @@
 <html>
           <?php
-          // print_r($chart_data)
+          // print_r($chart_data);
+				     //      foreach ($chart_data as $data) {
+							  // print("[\"".$data->name."\",new Date(".$data->userLoginTime."),new Date(".$data->userLogoutTime.")],\n");
+						   //        }
           ?>  <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-            google.charts.load('current', {'packages':['timeline']});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var container = document.getElementById('timeline');
-        var chart = new google.visualization.Timeline(container);
-        var dataTable = new google.visualization.DataTable();
+                  google.charts.load('43', {
+                      'packages': ['timeline']
+                  });
+                  google.charts.setOnLoadCallback(drawChart);
 
-        dataTable.addColumn({ type: 'string', id: 'President' });
-        dataTable.addColumn({ type: 'date', id: 'Start' });
-        dataTable.addColumn({ type: 'date', id: 'End' });
-        dataTable.addRows([
-          [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-          [ 'Washington', new Date(1890, 3, 30), new Date(1898, 2, 4) ],
-          [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-          [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ]]);
+                  function drawChart() {
+                      var container = document.getElementById('timeline');
+                      var chart = new google.visualization.Timeline(container);
+                      var dataTable = new google.visualization.DataTable();
 
-        chart.draw(dataTable);
-      }
+                      dataTable.addColumn({
+                          type: 'string',
+                          id: 'Name'
+                      });
+                      dataTable.addColumn({
+                          type: 'datetime',
+                          id: 'Start'
+                      });
+                      dataTable.addColumn({
+                          type: 'datetime',
+                          id: 'End'
+                      });
+                      dataTable.addRows([
+                          // ['Washington', new Date(1789, 3, 30), new Date(1797, 2, 4)],
+                          // ['Washington', new Date(1800, 3, 30), new Date(1810, 2, 4)],
+                          // ['Adams', new Date(1797, 2, 4), new Date(1801, 2, 4)],
+                          // ['Jefferson', new Date(1801, 2, 4), new Date(1809, 2, 4)]
+	                      <?php
+				          foreach ($chart_data as $data) {
+							  print("[\"".$data->name."\",new Date(".$data->userLoginTime."*1000),new Date(".$data->userLogoutTime."*1000)],\n");
+						          }
+						?>
+                      ]);
+                      chart.draw(dataTable);
+                  }
     </script>
   </head>
   <body>
