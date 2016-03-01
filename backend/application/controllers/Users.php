17153afb0410b3313a4ef50b2f118b63c3a68005
Nisham <moonblade168@gmail.com>
2016-03-01 13:11:54 +0530

    controls added

--- backend/application/controllers/Users.php
+++ backend/application/controllers/Users.php
@@ -6,15 +6,29 @@ class Users extends CI_Controller {
                 // $this->load->view('pages/manualPicker');
 		$data['chart_data'] = json_decode('[{"year":1,"time":10},{"year":2,"time":20},{"year":3,"time":30},{"year":4,"time":40}]',True);
                 $data['chart_data']=$this->getPeople();
+                $data['users']=$this->getMacs();
                 $this->load->view('pages/chart',$data);
         }
 
-        private function getPeople($from=24)
+        public function changeName()
+        {
+                $name = $this->input->get('name');
+        	$macId = $this->input->get('macId');
+        	$this->db->update('loggerUsers',array('name'=>$name),array('macId'=>$macId));
+                $this->index();
+        }
+
+        private function getPeople()
         {
                 $query = $this->db->query('select REPLACE(name,"Anonymous",userMacId) as name,UNIX_TIMESTAMP(userLoginTime) as userLoginTime, UNIX_TIMESTAMP(REPLACE(userLogoutTime,"0000-00-00 00:00:00",NOW())) as userLogoutTime from log,loggerUsers where log.userMacId = loggerUsers.macId
 ');
                 // header('Content-Type: application/json');
                 return $query->result();
         
+        }
+
+        private function getMacs()
+        {
+        	return $this->db->query("select * from loggerUsers")->result();
         }
 }
--- backend/application/views/pages/chart.php
+++ backend/application/views/pages/chart.php
@@ -1,50 +1,124 @@
 <html>
-          <?php
-          // print_r($chart_data);
-				     //      foreach ($chart_data as $data) {
-							  // print("[\"".$data->name."\",new Date(".$data->userLoginTime."),new Date(".$data->userLogoutTime.")],\n");
-						   //        }
-          ?>  <head>
+
+<head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-                  google.charts.load('43', {
-                      'packages': ['timeline']
-                  });
-                  google.charts.setOnLoadCallback(drawChart);
+    var CompleteData = [
+        <?php
+		foreach ($chart_data as $data) {
+				print("{name:\"".$data->name."\",start:new Date(".$data->userLoginTime."*1000),end:new Date(".$data->userLogoutTime."*1000)},\n");
+				}
+		?>
+    ]
 
-                  function drawChart() {
-                      var container = document.getElementById('timeline');
-                      var chart = new google.visualization.Timeline(container);
-                      var dataTable = new google.visualization.DataTable();
+    var users= [
+    		<?php
+    		foreach($users as $user)
+    		{
+    			print("{macId:\"".$user->macId."\",name:\"".$user->name."\"},\n");
+    		}
+		?>
+	]
+	function getName(macId) {
+		for (user in users)
+		{
+			user=users[user]
+			if(user.macId==macId)
+				return user.name
+		}
+		return "null"
+	}
+    dataForTable = CompleteData
+    function changeName(macId) {
+    	document.getElementById('nameSpace').value=getName(macId);
+    }
 
-                      dataTable.addColumn({
-                          type: 'string',
-                          id: 'Name'
-                      });
-                      dataTable.addColumn({
-                          type: 'datetime',
-                          id: 'Start'
-                      });
-                      dataTable.addColumn({
-                          type: 'datetime',
-                          id: 'End'
-                      });
-                      dataTable.addRows([
-                          // ['Washington', new Date(1789, 3, 30), new Date(1797, 2, 4)],
-                          // ['Washington', new Date(1800, 3, 30), new Date(1810, 2, 4)],
-                          // ['Adams', new Date(1797, 2, 4), new Date(1801, 2, 4)],
-                          // ['Jefferson', new Date(1801, 2, 4), new Date(1809, 2, 4)]
-	                      <?php
-				          foreach ($chart_data as $data) {
-							  print("[\"".$data->name."\",new Date(".$data->userLoginTime."*1000),new Date(".$data->userLogoutTime."*1000)],\n");
-						          }
-						?>
-                      ]);
-                      chart.draw(dataTable);
-                  }
+    function changeTime(hours) {
+        var fromDate = new Date()
+        fromDate.setHours(fromDate.getHours() - hours);
+        dataForTable = CompleteData.filter(function(row) {
+            return row.end > fromDate;
+        });
+        // for(var i=0;i<dataForTable.length;++i)
+        // {
+        // 	if(dataForTable[i].start<fromDate)
+        // 		dataForTable[i].start=fromDate
+        // }
+        drawChart()
+    }
+    google.charts.load('43', {
+        'packages': ['timeline']
+    });
+
+    google.charts.setOnLoadCallback(firstDraw);
+function firstDraw() {
+	changeTime(24)
+	drawChart()
+}
+    function drawChart() {
+        var container = document.getElementById('timeline');
+        var chart = new google.visualization.Timeline(container);
+        var dataTable = new google.visualization.DataTable();
+        dataTable.addColumn({
+            type: 'string',
+            id: 'Name'
+        });
+        dataTable.addColumn({
+            type: 'datetime',
+            id: 'Start'
+        });
+        dataTable.addColumn({
+            type: 'datetime',
+            id: 'End'
+        });
+        for (i = 0; i < dataForTable.length; ++i) {
+            tableRow = dataForTable[i]
+            dataTable.addRow([
+                tableRow.name, tableRow.start, tableRow.end
+            ]);
+
+        }
+        chart.draw(dataTable);
+    }
     </script>
-  </head>
-  <body>
-    <div id="timeline" style="width: 900px; height: 500px;"></div>
-  </body>
+</head>
+
+<body onLoad="changeName(users[0].macId)">
+    <div id="body" style="width: 100%; height: 100%;">
+        <div id="selectors" style="width: 100%; height: 5%;">
+            <div id="macChanger" style="float:left">
+            	 <form style="float:left" action="changeName">
+	            	<select name="macId" id="macSelector" style="float:left" onLoad="changeName(this.value)" onChange="changeName(this.value)">
+			    		<?php
+			    		foreach($users as $user)
+			    		{
+			    			print("<option value=\"".$user->macId."\">".$user->macId." (".$user->name.")</option>\n");
+			    		}
+						?>
+	            	</select>
+				  <input id="nameSpace" style="float:left" type="text" name="name">
+				  <input style="float:left" type="submit" value="Change Name">
+				</form> 
+            </div>
+            <div id="timeChanger" style="float:right">
+                <select onChange="changeTime(this.value)">
+                    <!-- <option value=""></option> -->
+                    <option value="1">Last hour</option>
+                    <option value="2">Last 2 hours</option>
+                    <option value="4">Last 4 hours</option>
+                    <option value="8">Last 8 hours</option>
+                    <option value="12">Last 12 hours</option>
+                    <option value="24" selected>Last 24 hours</option>
+                    <option value="48">Last 2 Days</option>
+                    <option value="96">Last 4 Days</option>
+                    <option value="168">Last Week</option>
+                    <option value="336">Last 2 Weeks</option>
+                    <option value="730">Last Month</option>
+                </select>
+            </div>
+        </div>
+        <div id="timeline" style="width: 100%; height: 95%;"></div>
+    </div>
+</body>
+
 </html>
